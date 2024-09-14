56cf14c8106f82b0ef01775147925e486e9c7ffb
iyuta1124 <iyuta1124@gmail.com>
2024-09-14 21:59:38 +0900

    applied news pages SSR

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import NewsList from "../_components/NewsList";
 import Pagination from "../_components/Pagination";
 import { NEWS_LIST_LIMIT } from "../_constants";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
