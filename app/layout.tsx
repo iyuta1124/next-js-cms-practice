b2a835685e946e41769e5cd347610df9d3811dc6
iyuta1124 <iyuta1124@gmail.com>
2024-09-15 12:57:03 +0900

    finished 10-1"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,24 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description: "テスト用のサイトです",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description: "テスト用のサイトです",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "http://localhost:3000",
+  },
+};
 
 export default function RootLayout({
   children,
